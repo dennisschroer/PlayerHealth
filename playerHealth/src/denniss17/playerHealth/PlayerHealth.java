6a662d670a4f7945b4159a2b3719eef72321864a
denniss17 <dennisschroer@hotmail.com>
2013-12-19 16:27:53 +0100

    TagAPI support, version updated

--- playerHealth/src/denniss17/playerHealth/PlayerHealth.java
+++ playerHealth/src/denniss17/playerHealth/PlayerHealth.java
@@ -1,10 +1,13 @@
 package denniss17.playerHealth;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scoreboard.Criterias;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 
@@ -12,7 +15,13 @@ import org.bukkit.scoreboard.Objective;
 public class PlayerHealth extends JavaPlugin {
 	public static VersionChecker versionChecker;
 	
+	public static final String OBJECTIVE_NAME = "playerhealth";
+	
+	public Map<Player, Set<String>> tags;
+	
 	public void onEnable(){
+		tags = new HashMap<Player, Set<String>>();
+		
 		// Register PlayerListener
 		this.getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
 		
@@ -27,18 +36,33 @@ public class PlayerHealth extends JavaPlugin {
 	
 	// Makes the health of other players visible for this player
 	public void showHealth(Player player){
-		Objective objective = player.getScoreboard().registerNewObjective("playerhealth", Criterias.HEALTH);
+		Objective objective = player.getScoreboard().registerNewObjective(OBJECTIVE_NAME, "dummy");
 		objective.setDisplayName(getConfig().getString("display_name"));
 		objective.setDisplaySlot(DisplaySlot.BELOW_NAME);
+		
+		// Send initial healths
 		for(Player other : getServer().getOnlinePlayers()){
-			other.setHealth(other.getHealth());
+			for(String tag : tags.get(other)){
+				objective.getScore(getServer().getOfflinePlayer(tag)).setScore((int) other.getHealth());
+			}
 		}
 	}
 	
 	// Makes the health of other players invisible for this player
 	public void removeHealth(Player player){
-		Objective objective = player.getScoreboard().getObjective("playerhealth");
+		Objective objective = player.getScoreboard().getObjective(OBJECTIVE_NAME);
 		if(objective!=null) objective.unregister();
+	}
+
+	public void sendHealthOfPlayer(Player player, int health){
+		Set<String> playerTags = tags.get(player);
+		Objective objective;
+		for(String tag : playerTags){
+			for(Player other : getServer().getOnlinePlayers()){
+				objective = other.getScoreboard().getObjective(OBJECTIVE_NAME);
+				if(objective!=null) objective.getScore(getServer().getOfflinePlayer(tag)).setScore(health);
+			}
+		}
 	}
 	
 	@Override
--- playerHealth/src/denniss17/playerHealth/PlayerListener.java
+++ playerHealth/src/denniss17/playerHealth/PlayerListener.java
@@ -1,10 +1,17 @@
 package denniss17.playerHealth;
 
+import java.util.HashSet;
+
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.kitteh.tag.PlayerReceiveNameTagEvent;
 
 public class PlayerListener implements Listener {
 	
@@ -22,6 +29,10 @@ public class PlayerListener implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onPlayerJoinAfter(PlayerJoinEvent event){
+		HashSet<String> set = new HashSet<String>();
+		set.add(event.getPlayer().getName());
+		plugin.tags.put(event.getPlayer(), set);
+		
 		if(event.getPlayer().hasPermission("playerhealth.*")){
 			if(PlayerHealth.versionChecker!=null && !PlayerHealth.versionChecker.latestVersion.equals(plugin.getDescription().getVersion())){
 				event.getPlayer().sendMessage("There is a new version of " + ChatColor.YELLOW + "PlayerHealth" + ChatColor.WHITE + " available!");
@@ -29,6 +40,33 @@ public class PlayerListener implements Listener {
 		}
 		if(event.getPlayer().hasPermission("playerhealth.show")){
 			plugin.showHealth(event.getPlayer());
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerQuit(PlayerQuitEvent event){
+		// Cleanup
+		plugin.tags.remove(event.getPlayer());
+	}
+	
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onNameTag(PlayerReceiveNameTagEvent event) {
+    	// A new tag is send -> add it to list
+    	plugin.tags.get(event.getNamedPlayer()).add(event.getTag());
+    }
+    
+    @EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerDamage(EntityDamageEvent event){
+		if(event.isCancelled()) return;
+		if(event.getEntity() instanceof Player){
+			plugin.sendHealthOfPlayer((Player)event.getEntity(), (int) (((Player)event.getEntity()).getHealth()-event.getDamage()));
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerHealthRegain(EntityRegainHealthEvent event){
+		if(event.getEntity() instanceof Player){
+			plugin.sendHealthOfPlayer((Player)event.getEntity(), (int) (((Player)event.getEntity()).getHealth()+event.getAmount()));
 		}
 	}
 }
